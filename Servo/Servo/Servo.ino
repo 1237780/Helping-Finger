81d93bdda416ffb2f765d861c4daccd2b16d5ad0
Jannis Kartenberg <78730290+1237780@users.noreply.github.com>
2021-02-28 22:15:41 +0100

    minor updates

--- Servo/Servo/Servo.ino
+++ Servo/Servo/Servo.ino
@@ -1,23 +1,18 @@
 #include <Servo.h> 
 Servo servoObject; 
 int position = 0;
-int posEin = 90;
-int posAus = 90; 
 const int waitTime = 5; 
  
 void setup() 
 { 
-  pinMode(D8, OUTPUT);
   pinMode(D0, OUTPUT);
   Serial.begin(115200);
   // Servo an digiteln Port D0
-  servoObject.attach(D0);
-  digitalWrite(D8, HIGH);  
+  servoObject.attach(D0);  
 } 
  
 void loop() 
-{ 
-  // Servo um 180 Grad drehen 
+{  
   for (position = 75; position < 95; position++) 
   { 
     servoObject.write(position); 
@@ -25,17 +20,10 @@ void loop()
   } 
   delay(1000);
  
-  // Servo wieder zurückdrehen 
   for (position = 95; position >= 75; position--) 
   { 
     servoObject.write(position); 
     delay(waitTime); 
   } 
   delay(1000);
-  //digitalWrite(D8, 0);
-  //Serial.println(digitalRead(D8));
-  //delay(3000);
-  //digitalWrite(D8, 1);
-  //delay(1000);
-  //Serial.println(digitalRead(D8));
 }
